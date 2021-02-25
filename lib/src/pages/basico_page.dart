e180e9c25c504d57c48bbb77fafdf4ddfb8e39fa
Antonio Arencibia <arencibiacasa@gmail.com>
2021-02-25 17:32:56 +0000

    Añado la imagen principal mediante enlace web

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -4,8 +4,13 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Básico Page'),
+      body: Column(
+        children: [
+          Image(
+            image: NetworkImage(
+                'https://www.iagua.es/sites/default/files/styles/amp_metadata_content_image_min_696px_wide/public/lago_portada.jpg?itok=syLMJNl4'),
+          )
+        ],
       ),
     );
   }
