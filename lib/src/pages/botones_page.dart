b2eb3a61bbfbe0f77b6c149ce2dc853171821a50
Antonio Arencibia <arencibiacasa@gmail.com>
2021-02-27 19:49:15 +0000

    Añadimos un gradiente de fondo

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class BotonesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          _fondoApp(),
+        ],
+      ),
+    );
+  }
+
+  Widget _fondoApp() {
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Colors.red,
+            Colors.blue,
+          ],
+        ),
+      ),
+    );
+    return Stack(
+      children: [
+        gradiente,
+      ],
+    );
   }
 }
